33a83a8ab2a6b053ca88c4cdab96708fdd8c4911
Ronald Arias <ronald.arias@thoughtworks.com>
2020-08-03 22:10:08 -0500

    Starts working with Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,9 +2,33 @@ import React from 'react';
 
 const Formulario = () => {
   return (
-    <div>
-      <h1>Formulario Aqui</h1>
-    </div>
+    <form action="submit" className="col-12">
+      <fieldset className="text-center">
+        <legend>Busca categorias por Categoria o Ingredientes</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Buscar por Ingrediente"
+          />
+        </div>
+        <div className="col-md-4">
+          <select name="categoria" id="categoria" className="form-control">
+            <option value="">-- Seleccione Categoria --</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
   );
 };
 
