25c6ebbed46b404e22f7996025b36c87514625d5
Ronald Arias <ronald.arias@thoughtworks.com>
2020-08-03 21:55:50 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <header className="bg-alert">
+        <h1>Busca recetas de bebidas</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
