3ea070e532e9f0e0c4673b88cc52cbf437aa8cf5
Ronald Arias <ronald.arias@thoughtworks.com>
2020-08-05 21:20:10 -0500

    Displaying Recipe photo and button to show more

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -5,6 +5,14 @@ const Receta = ({ recipe }) => {
     <div className="col-md-4 mb-3">
       <div className="card">
         <h2 className="card-header">{recipe.strDrink}</h2>
+        <img
+          className="card-img-top"
+          src={recipe.strDrinkThumb}
+          alt={`Imagen de ${recipe.strDrink}`}
+        />
+        <div className="card-body">
+          <button className="btn btn-block btn-primary">Ver Receta</button>
+        </div>
       </div>
     </div>
   );
