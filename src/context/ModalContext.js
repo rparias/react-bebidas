490309dd0d4782558a58b4dfc40a3921f8b599f5
Ronald Arias <ronald.arias@thoughtworks.com>
2020-08-06 21:39:23 -0500

    Getting recipe with id in API call and saving in state on ModalContext

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -5,6 +5,18 @@ export const ModalContext = createContext();
 
 const ModalProvider = (props) => {
   const [idRecipe, setIdRecipe] = useState(null);
+  const [recipe, setRecipe] = useState({});
+
+  // if we get the idRecipe, we can call the API
+  useEffect(() => {
+    const getRecipe = async () => {
+      if (!idRecipe) return;
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idRecipe}`;
+      const recipe = await axios.get(url);
+      setRecipe(recipe.data.drinks[0]);
+    };
+    getRecipe();
+  }, [idRecipe]);
 
   return (
     <ModalContext.Provider value={{ setIdRecipe }}>
