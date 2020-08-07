33ca7145fa969569f0c61095f833c75dc69484e8
Ronald Arias <ronald.arias@thoughtworks.com>
2020-08-06 22:07:08 -0500

    Installs material-ui library and using modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,11 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ModalContext } from '../context/ModalContext';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Receta = ({ recipe }) => {
   const { setIdRecipe } = useContext(ModalContext);
 
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setIdRecipe(null);
+  };
+
   const handleOnClick = () => {
     setIdRecipe(recipe.idDrink);
+    handleOpen();
   };
 
   return (
@@ -21,6 +59,11 @@ const Receta = ({ recipe }) => {
           <button className="btn btn-block btn-primary" onClick={handleOnClick}>
             Ver Receta
           </button>
+          <Modal open={open} onClose={handleClose}>
+            <div style={modalStyle} className={classes.paper}>
+              <h1>Desde Modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
